cc85cd3b3ddb09866050ea45d41e6ea3b4232a5d
PawanRoy1997 <pawanroy0066@gmail.com>
2022-12-30 22:30:43 +0530

    Initial Project

---
+++ app/src/main/java/PawanRoy1997/otpfield/MainActivity.kt
@@ -0,0 +1,11 @@
+package PawanRoy1997.otpfield
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
